d63e454402f7611e12e35ec7ddad580a9695efe9
kadiiravsaar <kadiiravsar@gmail.com>
2023-11-09 19:38:20 +0300

    Math Folder Added

---
+++ CSharp/Lesson3/Math/Program.cs
@@ -0,0 +1,5 @@
+﻿using Math;
+
+Transactions operations = new Transactions();
+operations.Sum(5, 6);
+operations.Sum(10,5);
---
+++ CSharp/Lesson3/Math/Transactions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    public class Transactions
+    {
+        public void Sum(int num1, int num2)
+        {
+            int total = num1 + num2;
+            Console.WriteLine("Total: " + total);
+        }
+    }
+}
