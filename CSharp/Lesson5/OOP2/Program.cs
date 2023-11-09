2bcb6567c640c8817f23df3903ef0a694f1389f3
kadiiravsaar <kadiiravsar@gmail.com>
2023-11-09 22:17:32 +0300

    Lesson 5 Added

---
+++ CSharp/Lesson5/OOP2/Program.cs
@@ -0,0 +1,23 @@
+﻿using OOP2;
+
+IndividualCustomer customer1 = new IndividualCustomer();
+customer1.FirstName = "Kadir";
+customer1.LastName = "Avşar";
+customer1.TcNo = "12345678910";
+
+
+
+CoorporateCustomer customer2 = new CoorporateCustomer();
+customer2.Id = 1;
+customer2.CompanyName = "İstanbulKodluyor";
+customer2.CustomerNumber = "616161"; // Ata class(Customer) geldi
+customer2.TaxNo = "61616161";
+
+Customer customer3 = new IndividualCustomer();
+Customer customer4 = new CoorporateCustomer();
+
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add(customer3);
+customerManager.Add(customer2);
+
---
+++ CSharp/Lesson5/OOP3/Program.cs
@@ -0,0 +1,35 @@
+﻿using OOP3;
+
+ICreditManager personalFinanceCredit = new PersonalFinanceCreditManager();
+personalFinanceCredit.Calculate();
+
+ICreditManager transportCredit = new TransportCreditManager();
+
+ICreditManager mortgageCreditManager = new MortgageCreditManager();
+
+ICreditManager tradesmanCreditManager = new TradesmanCreditManager();
+
+ApplicationManager applicationManager = new ApplicationManager();
+//applicationManager.Apply(personalFinanceCredit);
+
+
+
+List<ICreditManager> credits = new List<ICreditManager>() { personalFinanceCredit, transportCredit };
+
+Console.WriteLine("********************************************");
+
+
+
+applicationManager.Apply(personalFinanceCredit,
+    new List<ILoggerService>
+    {
+        new DatabaseLoggerService(),
+        new SmsLoggerService()}
+    );
+
+applicationManager.Apply(tradesmanCreditManager, 
+    new List<ILoggerService> 
+    { 
+        new FileLoggerService()
+    }
+    );
