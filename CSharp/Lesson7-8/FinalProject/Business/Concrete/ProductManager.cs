4fc33271ac472170f682c045497cb64b730e42a3
kadiiravsaar <kadiiravsar@gmail.com>
2023-11-11 02:32:42 +0300

    Lesson 7 And Lesson 8

--- CSharp/Lesson7-8/FinalProject/Business/Concrete/ProductManager.cs
+++ CSharp/Lesson7-8/FinalProject/Business/Concrete/ProductManager.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
+    //Business rules
+    
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
@@ -18,16 +20,19 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+
         public List<Product> GetAll()
         {
-            //Business rules
+            
             return _productDal.GetAll();
         }
+
 
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
         }
+
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
         {
