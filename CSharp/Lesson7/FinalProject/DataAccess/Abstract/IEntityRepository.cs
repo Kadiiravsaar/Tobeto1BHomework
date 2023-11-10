5fc9b09ca06fe48b1e644c1cd86b555fd779fed9
kadiiravsaar <kadiiravsar@gmail.com>
2023-11-11 02:09:12 +0300

    Lesson 8 added

--- CSharp/Lesson7/FinalProject/DataAccess/Abstract/IEntityRepository.cs
+++ CSharp/Lesson7/FinalProject/DataAccess/Abstract/IEntityRepository.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
+        // class => Referans tip olabilir
+
+        List<T> GetAll(Expression<Func<T, bool>> filter = null); // delege
+        T Get(Expression<Func<T, bool>> filter); // tek data getirmek için
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
