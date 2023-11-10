7a08ed1239296adc91b34b2778c54635b879536d
kadiiravsaar <kadiiravsar@gmail.com>
2023-11-11 01:57:39 +0300

    Lesson 7 added

---
+++ CSharp/Lesson7/FinalProject/ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+foreach (var prodManagers in productManager.GetAll())
+{
+    Console.WriteLine(prodManagers.ProductName);
+}
+
+ProductManager productManager1 = new ProductManager(new EfProductDal());
+
+foreach (var prodManagers1 in productManager1.GetAll())
+{
+    Console.WriteLine(prodManagers1.ProductName);
+}
