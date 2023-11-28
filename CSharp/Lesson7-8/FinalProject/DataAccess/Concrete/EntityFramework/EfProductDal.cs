8933942ba892b07b47fe2bc7a13d3cb3ed2b1d93
kadiiravsaar <kadiiravsar@gmail.com>
2023-11-28 21:35:48 +0300

    revize

--- CSharp/Lesson7-8/FinalProject/DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ CSharp/Lesson7-8/FinalProject/DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -4,7 +4,7 @@ using Entities.Concrete;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : EfEntityRepositoryBase<Category, AppDbContext>, ICategoryDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, AppDbContext>, IProductDal
     {
        
     }
