314bdb09d2b2cdcb0bc238e1bf2f24b9cfb717fd
kadiiravsaar <kadiiravsar@gmail.com>
2023-11-28 22:57:08 +0300

    Business Category Added

---
+++ CSharp/Lesson7-8/FinalProject/Business/Concrete/CategoryService.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstract;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public IResult Add(Category category)
+        {
+            //ValidationTool.Validate(new CategoryValidator(), category);
+
+            _categoryDal.Add(category);
+            return new SuccessResult("Eklendi");
+        }
+
+        public IResult Delete(Category category)
+        {
+            _categoryDal.Delete(category);
+            return new SuccessResult("Silindi");
+        }
+
+        public IDataResult<List<Category>> GetAll()
+        {
+            return new SuccessDataResult<List<Category>>(_categoryDal.GetAll(),"Listelendi");
+        }
+
+        public IDataResult<Category> GetById(int id)
+        {
+            return new SuccessDataResult<Category>(_categoryDal.Get(x => x.CategoryId == id),"Id bazlı data getirme");
+        }
+
+        public IResult Update(Category category)
+        {
+            _categoryDal.Update(category);
+            return new SuccessResult("Güncellendi");
+
+        }
+    }
+}
